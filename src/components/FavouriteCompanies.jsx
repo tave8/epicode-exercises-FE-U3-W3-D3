a5b93392f7c9f59a6e495d91bd4d907390e1a131
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-18 14:14:09 +0100

    add favourite companies & home links

--- src/components/FavouriteCompanies.jsx
+++ src/components/FavouriteCompanies.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+import { Container, Row, Col, Form } from "react-bootstrap"
+
+// redux stuff
+import { useSelector } from "react-redux"
+
 const FavouriteCompanies = () => {
-    return (
-        <></>
-    )
+  return (
+    <Container className="mt-3">
+      <Row>
+        <Col>
+          <Link to="/">Home</Link>
+        </Col>
+      </Row>
+      <Row></Row>
+    </Container>
+  )
 }
 
 export default FavouriteCompanies
